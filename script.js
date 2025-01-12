f0e4bd2637cb2ce24e3a2463f5d1a9624ff5f7c2
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-12 16:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const promise1 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("promise1");
+	}, 1000);
+})
+
+const promise2 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("promise2");
+	}, 2000);
+})
+
+const promise3 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("promise3");
+	}, 3000);
+})
+
+Promise.all([promise1, promise2, promise3]).then((data) => {
+	console.log(data);
+}).catch((r) => {
+  console.log(r);
+})
+
+
+
+
